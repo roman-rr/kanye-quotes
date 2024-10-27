be3e2c61ac3c8940a388573567cca8d62ab758a1
Roman <romwtb@gmail.com>
2024-10-27 13:33:18 +0700

    Update side.blade.php

--- resources/views/layouts/side.blade.php
+++ resources/views/layouts/side.blade.php
@@ -5,8 +5,8 @@
     <base href="../../../"/>
     <title>{{ config('app.name') }} – Auth</title>
     <meta charset="utf-8" />
-    <meta name="description" content="The most advanced Bootstrap Admin Theme on Themeforest trusted by 100,000 beginners and professionals. Multi-demo, Dark Mode, RTL support and complete React, Angular, Vue, Asp.Net Core, Blazor, Django, Flask & Laravel versions. Grab your copy now and get life-time updates for free." />
-    <meta name="keywords" content="metronic, bootstrap, bootstrap 5, angular, VueJs, React, Asp.Net Core, Blazor, Django, Flask & Laravel starter kits, admin themes, web design, figma, web development, free templates, free admin themes, bootstrap theme, bootstrap template, bootstrap dashboard, bootstrap dak mode, bootstrap button, bootstrap datepicker, bootstrap timepicker, fullcalendar, datatables, flaticon" />
+    <meta name="description" content="Highly scalable, mobile-friendly Laravel platform with authentication, RESTful API, Metronic theme integration, Livewire, comprehensive tests, and more." />
+    <meta name="keywords" content="Laravel, Livewire, restful, swagger, metronic, php" />
     <meta name="viewport" content="width=device-width, initial-scale=1" />
     <meta name="csrf-token" content="{{ csrf_token() }}">
     <meta property="og:locale" content="{{app()->getLocale()}}" />
